06ba9d4ec3ddc33818999825dc8ebe71f64e0482
Milllll000 <aulann@outlook.com>
2025-03-28 13:47:24 -0600

    Se puede hacer conexión

---
+++ DemoAllan/Assets/Scripts/ConectarServidor.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.Networking;
+
+
+public class ConectarServidor : MonoBehaviour
+{
+    public struct Hora
+    {
+        public string hora;
+    }
+
+    void OnEnable()
+    {
+        EnviarDatosJSON();
+    }
+
+    private void EnviarDatosJSON()
+    {
+        StartCoroutine(SubirDatosJSON());
+    }
+
+    private IEnumerator SubirDatosJSON()
+    {
+        Hora prueba;
+        prueba.hora = "Hola";
+
+        string horaJSON = JsonUtility.ToJson(prueba);
+
+        UnityWebRequest request = UnityWebRequest.Post("http://127.0.0.1", horaJSON, "application/JSON");
+
+        yield return request.SendWebRequest();
+
+        // Después de un rato
+        if (request.result == UnityWebRequest.Result.Success)
+        {
+            print("\nConexión exitosa.\n\n");
+        }
+        else
+        {
+            print("Error de conexión: " + request.responseCode);
+        }
+    }
+}
